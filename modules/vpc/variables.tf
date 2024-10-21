cd3b33b534846d28a5edd7f19e3054c17ea71d1a
Israel Batista <israelbatiista19@gmail.com>
2024-10-21 19:40:02 -0300

    feat: create module of VPC

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,10 @@
+
+variable "prefix" {
+  description = "Prefix for the resources"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
